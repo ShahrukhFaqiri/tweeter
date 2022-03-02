6bf381355e1e02f1458464d6148c453a7bbb8a89
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-02 04:31:52 +0000

    Remove random spaces for readibility

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 $(() => {
   const $tweetText = $('#tweet-text');
-  $tweetText.on('input', function() {
+  $tweetText.on('input', function () {
     //this.value.length could also work
     let input = $tweetText.val().length;
     let letCount = 140 - input;
     const $counter = $(this).siblings().children().last();
     //Adds or Remove based on if let count is below 0
-    letCount < 0 ? $counter.addClass('exeeded-char-limit') : $counter.removeClass('exeeded-char-limit'); 
+    letCount < 0
+      ? $counter.addClass('exeeded-char-limit')
+      : $counter.removeClass('exeeded-char-limit');
     $counter.text(letCount);
   });
 });
