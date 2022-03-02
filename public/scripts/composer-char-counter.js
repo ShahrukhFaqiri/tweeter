1ee3450ea2f301f841693769712164f7f287ec2e
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-02 02:36:52 +0000

    Add tweets list, add css and style to it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(() => {
   const $tweetText = $('#tweet-text');
   $tweetText.on('input', function() {
+    //this.value.length could also work
     let input = $tweetText.val().length;
     let letCount = 140 - input;
     const $counter = $(this).siblings().children().last();
-    letCount < 0 ? $counter.addClass('exeeded-char-limit') : $counter.removeClass('exeeded-char-limit');
+    //Adds or Remove based on if let count is below 0
+    letCount < 0 ? $counter.addClass('exeeded-char-limit') : $counter.removeClass('exeeded-char-limit'); 
     $counter.text(letCount);
   });
 });
