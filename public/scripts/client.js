cc14e4d30edc3cf5e1a0058c463bffca93bececa
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-03 01:59:07 +0000

    Add Ajax Post Request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -24,6 +24,7 @@ $(() => {
     },
   ];
 
+  // Looping through database, passing and appending createTweetElement to HTML
   const renderTweets = (tweets) => {
     for (let tweet of tweets) {
       // $(createTweetElement(tweet)).appendTo('.tweets');
@@ -31,37 +32,54 @@ $(() => {
     }
   };
 
+  // Call back for RenderTweet to make Dynamic Data
   const createTweetElement = (data) => {
     let $tweet = `  
-  <article id="tweet-card">
-    <header>
-      <div class="header">
-        <div>
-          <img src="${data.user.avatars}" />
-          <span class="user-identity">
-            <label id="display-name">${data.user.name}</label>
-            <label class="grayed-out">${data.user.handle}</label>
-          </span>
+    <article id="tweet-card">
+      <header>
+        <div class="header">
+          <div>
+            <img src="${data.user.avatars}" />
+            <span class="user-identity">
+              <label id="display-name">${data.user.name}</label>
+              <label class="grayed-out">${data.user.handle}</label>
+            </span>
+        </div>
+      </header>
+      <div class="user-tweet">
+        <p>${data.content.text}</p>
       </div>
-    </header>
-    <div class="user-tweet">
-      <p>${data.content.text}</p>
-    </div>
-    <footer>
-      <div class="footer">
-        <span>${timeago.format(data.created_at)}</span>
-        <div>
-          <div class="icons">
-            <i class="fa-solid fa-flag hover-icon"></i>
-            <i class="fa-solid fa-retweet hover-icon"></i>
-            <i class="fa-solid fa-heart hover-icon"></i>
+      <footer>
+        <div class="footer">
+          <span>${timeago.format(data.created_at)}</span>
+          <div>
+            <div class="icons">
+              <i class="fa-solid fa-flag hover-icon"></i>
+              <i class="fa-solid fa-retweet hover-icon"></i>
+              <i class="fa-solid fa-heart hover-icon"></i>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-</article>`;
+      </footer>
+  </article>`;
     return $tweet;
   };
 
   renderTweets(data);
+
+  // Ajax POST request from form to DB
+  $('.form-class').on('submit', function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $(this).serialize(),
+    })
+      .then(function () {
+        console.log('it worked');
+      })
+      .catch(function (error) {
+        console.log(`There was an error`, error);
+      });
+  });
 });
