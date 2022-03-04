c6663e1ce312311baa6689bbaea7542daf220f9c
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-04 05:37:48 +0000

    Enchance readiblity

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,6 @@
 $(() => {
-  const $tweetText = $('#tweet-text');
-  $tweetText.on('input', function () {
-    //this.value.length could also work
-    let input = $tweetText.val().length;
+  $('#tweet-text').on('input', function () {
+    let input = $('#tweet-text').val().length;
     let letCount = 140 - input;
     const $counter = $(this).siblings().children().last();
     //Adds or Remove based on if let count is below 0
@@ -10,8 +8,9 @@ $(() => {
       ? $counter.addClass('exeeded-char-limit')
       : $counter.removeClass('exeeded-char-limit');
     //Adds class based on if the character limit is exceeded or not
-    letCount < 0 ? $('#error').removeClass('hidden') : $('#error').addClass('hidden');
-
+    letCount < 0 
+      ? $('#error').removeClass('hidden') 
+      : $('#error').addClass('hidden');
     $counter.text(letCount);
   });
 });
